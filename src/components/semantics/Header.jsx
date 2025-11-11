8771571fb273809d53d9db3d533f9a25842cf648
LuvAgrawal <la24092002@gmail.com>
2025-11-11 14:28:50 +0530

    navigation fix

--- src/components/semantics/Header.jsx
+++ src/components/semantics/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
         <>
             <header className='sticky top-0 left-0 z-50 w-full flex items-center justify-between py-2 px-4 shadow-md rounded-b-lg bg-white'>
                 <div className="logo">
-                    <img src={logo} className='h-[10dvh]' alt="Wedding Bells Logo" />
+                    <a href='/'><img src={logo} className='h-[10dvh]' alt="Wedding Bells Logo" /></a>
                 </div>
                 <div className="nav hidden lg:block">
                     <ul className='flex items-center justify-center'>
@@ -26,7 +26,7 @@ const Header = () => {
                     <ul className='flex items-center justify-center'>
                         {socialsData.map((item, index) => (
                             <li key={index} className='px-2'>
-                                <Link to={item.link} target="_blank" rel="noopener noreferrer">{item.logo}</Link>
+                                <a href={item.link} target="_blank" rel="noopener noreferrer">{item.logo}</a>
                             </li>
                         ))}
                     </ul>
@@ -38,7 +38,7 @@ const Header = () => {
             {isOpen && <ul className='fixed z-999 top-[13dvh] w-full uppercase font-[Roboto Slab] h-screen bg-[#00000055] lg:hidden' onClick={() => setIsOpen(false)}>
                 {navData.map((item, index) => (
                     <li key={index} className='bg-[#f8f8f8] p-2 border-b font-semibold border-gray-300'>
-                        <Link to={item.link}>{item.title}</Link>
+                        <a href={item.link}>{item.title}</a>
                     </li>
                 ))}
             </ul>}
